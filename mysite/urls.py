560fc7fa5fc3bd0941bb816ebb86f08c1b6e72b8
httle <751160121@qq.com>
2020-05-25 22:25:53 +0800

    url修改

--- mysite/urls.py
+++ mysite/urls.py
@@ -38,4 +38,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.BLOGIMG_URL, document_root = settings.BLOGIMG_ROOT)
+# urlpatterns += static(settings.BLOGIMG_URL, document_root = settings.BLOGIMG_ROOT)
