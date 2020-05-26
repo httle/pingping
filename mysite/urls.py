4f82a1f811ee18ccf4fb52dcedf15e931de4aaad
httle <751160121@qq.com>
2020-05-26 20:56:47 +0800

    url修改3

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from . import views
 urlpatterns = [
     path('',views.home2,name='home'),
