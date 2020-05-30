66549aa99b5c4c1a8747bb24090878369c76ffbb
httle <751160121@qq.com>
2020-05-30 19:40:24 +0800

    发完信息后立即关闭

--- machine_control/views.py
+++ machine_control/views.py
@@ -30,8 +30,9 @@ def appControl(request):
         }
     }
     data_json = json.dumps(data)
-    p = Process(target = socket_web,args=(data_json,))
-    p.start()
+    socket_web(data_json)
+    # p = Process(target = socket_web,args=(data_json,))
+    # p.start()
     # socket_web(data_json)
     # socket_data(data_json)
     # print(data)
@@ -55,11 +56,12 @@ def socket_web(data_json):
     my_socket.listen(1)
     conn,addr = my_socket.accept()
     conn.send(data_json.encode())
-    while True:
-        time.sleep(2)
-        rec = conn.recv(1024)
-        msg = rec.decode()
-        print(msg)
-        if msg == "end":
-            break
+    # while True:
+    #     time.sleep(2)
+    #     rec = conn.recv(1024)
+    #     msg = rec.decode()
+    #     print(msg)
+    #     if msg == "end":
+    #         break
+    time.sleep(1)
     conn.close()
