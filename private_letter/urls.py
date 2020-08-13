22de10310f5e5ab3dcba3ac861548d8744c9d2d3
httle <751160121@qq.com>
2020-08-13 09:16:30 +0800

    modify

--- private_letter/urls.py
+++ private_letter/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     path('',views.privateLetter,name='privateLetter'),
     path('chatMessage/',views.chatMessage,name='chatMessage'),
     path('sendChatMes/',views.sendChatMes,name='sendChatMes'),
+    path('webNotify/',views.webChatNotify,name='webNotify'),
     path('appChatList',views.appChatList),
     path('appDetailChat',views.appDetailChat),
     path('appChatSend',views.appChatSend),
